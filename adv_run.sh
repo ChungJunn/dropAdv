381b8fa9ed18b211d931f06e7125498ebd6cf672
Chungjun Lee <chunglee3224@gmail.com>
2020-10-21 23:47:53 +0900

    ready for exp25 performance

--- adv_run.sh
+++ adv_run.sh
@@ -6,36 +6,37 @@ export CUDA_VISIBLE_DEVICES=$1
 DATASET='mnist' # mnist or cifar10
 MODEL='lenet' # base, small, or large (for cifar10) || lenet, modelA, or modelB (for mnist)
 LR=0.001
-NUM_EPOCHS=1
+NUM_EPOCHS=1000
 BATCH_SIZE=64
 EPSILON=0.3
 ITERATION=40
 ALPHA=0.5
 DROP_P=0.0
-PATIENCE=20
+PATIENCE=10
 
 # adversarial training
-ADV_TRAIN=1
+ADV_TRAIN=0
 
 # neptune
-NAME='testing-ifgsm'
-TAG='testing'
+NAME='exp25'
+TAG='testing-performance-of-different-training-methods'
 
-for i in 1
+for i in 1 2 3 
 do
     LOAD_ADV_TEST=0
-    ADV_TEST_OUT_PATH=$HOME'/dropAdv/data/'$DATASET'-'$MODEL'-eps'$EPSILON'-drop_p'$DROP_P'.run'$i'.ae'
+    #ADV_TEST_OUT_PATH=$HOME'/dropAdv/data/'$DATASET'-'$MODEL'-eps'$EPSILON'-drop_p'$DROP_P'.run'$i'.ae'
+    ADV_TEST_OUT_PATH=$HOME'/dropAdv/data/dummy.ae'
 
-    #LOAD_ADV_TEST=1
-    #ADV_TEST_PATH1=$HOME'/dropAdv/data/mnist-lenet-eps'$EPSILON'-drop_p0.run1.ae'
-    #ADV_TEST_PATH2=$HOME'/dropAdv/data/mnist-lenet-eps'$EPSILON'-drop_p0.run2.ae'
-    #ADV_TEST_PATH3=$HOME'/dropAdv/data/mnist-lenet-eps'$EPSILON'-drop_p0.run3.ae'
-    #ADV_TEST_PATH4=$HOME'/dropAdv/data/mnist-modelA-eps'$EPSILON'-drop_p0.run1.ae'
-    #ADV_TEST_PATH5=$HOME'/dropAdv/data/mnist-modelA-eps'$EPSILON'-drop_p0.run2.ae'
-    #ADV_TEST_PATH6=$HOME'/dropAdv/data/mnist-modelA-eps'$EPSILON'-drop_p0.run3.ae'
-    #ADV_TEST_PATH7=$HOME'/dropAdv/data/mnist-modelB-eps'$EPSILON'-drop_p0.run1.ae'
-    #ADV_TEST_PATH8=$HOME'/dropAdv/data/mnist-modelB-eps'$EPSILON'-drop_p0.run2.ae'
-    #ADV_TEST_PATH9=$HOME'/dropAdv/data/mnist-modelB-eps'$EPSILON'-drop_p0.run3.ae'
+    LOAD_ADV_TEST=1
+    ADV_TEST_PATH1=$HOME'/dropAdv/data/mnist-lenet-eps'$EPSILON'-drop_p0.0.run1.ae'
+    ADV_TEST_PATH2=$HOME'/dropAdv/data/mnist-lenet-eps'$EPSILON'-drop_p0.0.run2.ae'
+    ADV_TEST_PATH3=$HOME'/dropAdv/data/mnist-lenet-eps'$EPSILON'-drop_p0.0.run3.ae'
+    ADV_TEST_PATH4=$HOME'/dropAdv/data/mnist-modelA-eps'$EPSILON'-drop_p0.0.run1.ae'
+    ADV_TEST_PATH5=$HOME'/dropAdv/data/mnist-modelA-eps'$EPSILON'-drop_p0.0.run2.ae'
+    ADV_TEST_PATH6=$HOME'/dropAdv/data/mnist-modelA-eps'$EPSILON'-drop_p0.0.run3.ae'
+    ADV_TEST_PATH7=$HOME'/dropAdv/data/mnist-modelB-eps'$EPSILON'-drop_p0.0.run1.ae'
+    ADV_TEST_PATH8=$HOME'/dropAdv/data/mnist-modelB-eps'$EPSILON'-drop_p0.0.run2.ae'
+    ADV_TEST_PATH9=$HOME'/dropAdv/data/mnist-modelB-eps'$EPSILON'-drop_p0.0.run3.ae'
 
     python3 cifar10.py \
         --dataset=$DATASET \
